18f12430a4935430a19c83929d24e1155514277a
SherzodbekDeveloper <omilxanovs7@gmail.com>
2024-08-04 10:40:24 +0500

    asdsadasd

--- index.js
+++ index.js
@@ -18,3 +18,42 @@ window.onscroll = () =>{
     menu.classList.remove('bx-x')
     header.classList.remove('active')
 }
+
+//bot token
+var telegram_bot_id = "7298772287:AAFqF9GuuuAhrGQDP-WBCo0WZm2Bb4KOzaA";
+//chat id
+var chat_id = '@sherzodeveloperMessages';
+var u_name, email, message;
+var ready = function () {
+   
+    u_name = document.getElementById("name").value;
+    email = document.getElementById("email").value;
+    message = document.getElementById("message").value;
+    message = "Name: " + u_name + "\nEmail: " + email + "\nMessage: " + message;
+};
+var sender = function () {
+    ready();
+    let p = document.getElementById('p')
+    p.innerHTML = "Malumot Yuborildi✅"
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://api.telegram.org/bot" + telegram_bot_id + "/sendMessage",
+        "method": "POST",
+        "headers": {
+            "Content-Type": "application/json",
+            "cache-control": "no-cache"
+        },
+        "data": JSON.stringify({
+            "chat_id": chat_id,
+            "text": message
+        })
+    };
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+    });
+    document.getElementById("name").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("message").value = "";
+    return false;
+};
